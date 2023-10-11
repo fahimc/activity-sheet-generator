a2848c1a9ea09026800911a90233b0458709026f
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2023-10-11 13:32:27 +0100

    maths sheets complete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,174 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import { Container } from "./components/Container";
-import { useEffect } from "react";
 
-function App() {
-  function generatePDF() {
-    var opt = {
-      margin: 1,
-      filename: "myfile.pdf",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: {
-        dpi: 300,
-        letterRendering: true,
-        width: 840,
-        height: 1188,
-      },
-      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-    };
-    // Choose the element that your content will be rendered to.
-    const element = document.querySelector(".page-container");
-    // Choose the element and save the PDF for your user.
-    window.html2pdf().set(opt).from(element).save();
+const numberOfSheets = 30;
+let currentOperator = "+";
+let pages = generateProgressiveMathQuestions(numberOfSheets, currentOperator);
+
+function getRandomOperator() {
+  const num = randomIntFromInterval(0, 3);
+  switch (num) {
+    case 0:
+      return "+";
+    case 1:
+      return "-";
+    case 2:
+      return "x";
+    case 3:
+      return "/";
+    default:
+      return "+";
+  }
+}
+
+function getOperatorTitle(operator) {
+  switch (operator) {
+    case "+":
+      return "Addition";
+    case "-":
+      return "Subtraction";
+    case "x":
+      return "Multiplication";
+    case "/":
+      return "Division";
+    default:
+      return undefined;
+  }
+}
+
+function generateMathQuestionForKids(maxNumber, operator) {
+  // Increase the range of numbers as the sheets progress
+
+  let operand1 = randomIntFromInterval(1, maxNumber);
+  let operand2 = randomIntFromInterval(1, maxNumber);
+  // Ensure the division questions are solvable and the result is a whole number
+  if (operator === "/") {
+    operand2 = operand2 === 0 ? 1 : operand2; // Avoid division by zero
+    const nextOp = operand1 * operand2;
+    operand1 = nextOp <= 10 ? nextOp : 10; // Ensure the result is a whole number
   }
 
+  // Ensure subtraction questions do not result in negative numbers
+  if (operator === "-" && operand1 < operand2) {
+    const temp = operand1;
+    operand1 = operand2;
+    operand2 = temp;
+  }
+
+  const question = `${operand1} ${operator} ${operand2} = `;
+
+  return {
+    operand1: operand1,
+    operand2: operand2,
+    operator: operator,
+    question: question,
+  };
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateProgressiveMathQuestions(numSheets, operator) {
+  const item = [];
+
+  for (let i = 1; i <= numSheets; i++) {
+    let maxNumber = randomIntFromInterval(i <= 10 ? i : 10, 10);
+    const questions = [];
+    for (let a = 1; a <= 12; a++) {
+      let op = operator || getRandomOperator();
+      let quest = generateMathQuestionForKids(maxNumber, op);
+      while (questions.find((obj) => obj.question === quest.question)) {
+        maxNumber = maxNumber = randomIntFromInterval(i <= 10 ? i : 10, 10);
+        quest = generateMathQuestionForKids(maxNumber, op);
+      }
+      questions.push(quest);
+    }
+    item.push(questions);
+  }
+
+  return item;
+}
+
+function generatePDF() {
+  var opt = {
+    margin: 1,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    pagebreak: {
+      mode: "css",
+    },
+    html2canvas: {
+      dpi: 300,
+      letterRendering: true,
+      width: 840,
+      height: 1188 * numberOfSheets,
+    },
+    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+  };
+  // Choose the element that your content will be rendered to.
+  const element = document.querySelector(".book");
+  // Choose the element and save the PDF for your user.
+  window.html2pdf().set(opt).from(element).save();
+}
+
+function App() {
+  const [currentPages, setCurrentPages] = useState(pages);
   return (
     <>
-      <button onClick={() => generatePDF()}>pdf</button>
-      <div className="page-container">
-        <div className="pattern"></div>
-        <div className="page">
-          <div>
-            <h1>
-              {" "}
-              <img className="bird" src="assets/images/bird.png"></img> Maths
-              Worksheet
-              <img className="bird2" src="assets/images/bird.png"></img>
-            </h1>
-            <h2>Additions</h2>
-          </div>
-          <Container />
-          <div className="bottom">
-            <img src="assets/images/bottom2.png"></img>
-          </div>
+      <select
+        onChange={(event) => {
+          currentOperator = event.target.value;
+          pages = generateProgressiveMathQuestions(
+            numberOfSheets,
+            currentOperator
+          );
+          setCurrentPages(pages);
+        }}
+      >
+        <option value={"+"}>addition</option>
+        <option value={"-"}>subtraction</option>
+        <option value={"x"}>multiplication</option>
+        <option value={"/"}>division</option>
+        <option value={""}>mixed</option>
+      </select>
+      <div className="book">
+        <div className="page-container">
+          <img className="title-cover" src="assets/images/title.png"></img>
+        </div>
+        {currentPages.map((currentPage, key) => {
+          return (
+            <div className="page-container" key={key}>
+              <div className="pattern"></div>
+              <div className="page">
+                <div>
+                  <h1>
+                    {" "}
+                    <img
+                      className="bird"
+                      src="assets/images/bird.png"
+                    ></img>{" "}
+                    Maths Worksheet
+                    <img className="bird2" src="assets/images/bird.png"></img>
+                  </h1>
+                  <h2>{getOperatorTitle(currentOperator)}</h2>
+                </div>
+                <Container items={currentPage} />
+                <div className="bottom">
+                  <img src="assets/images/bottom2.png"></img>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="page-container">
+          <img
+            style={{ pageBreakAfter: "before" }}
+            className="title-cover"
+            src="assets/images/colouring-back.png"
+          ></img>
         </div>
       </div>
     </>
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,39 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { MathBlock } from "./MathBlock";
 
-function generateMathQuestionForKids(operator) {
-  // Generate two random numbers between 1 and 10
-  const operand1 = Math.floor(Math.random() * 10) + 1;
-  const operand2 = Math.floor(Math.random() * 10) + 1;
-
-  // If no operator is provided, choose a random operator (+ or -)
-  if (!operator) {
-    const operators = ["+", "-"];
-    operator = operators[Math.floor(Math.random() * operators.length)];
-  }
-
-  // Ensure the result is not negative for simplicity for 5-year-olds
-  let question;
-  if (operator === "-" && operand1 < operand2) {
-    question = `${operand2} ${operator} ${operand1} = `;
-  } else {
-    question = `${operand1} ${operator} ${operand2} = `;
-  }
-
-  // Return an object containing each part of the question
-  return {
-    operand1: operand1,
-    operand2: operand2,
-    operator: operator,
-    question: question,
-  };
-}
-
-// Test the function
-const mathQuestion = generateMathQuestionForKids();
-console.log(mathQuestion);
-
-export function Container() {
+export function Container(props) {
   const containerRef = useRef(null);
   let contentHeight = 0;
   const numberOfRows = 6;
@@ -43,8 +11,8 @@ export function Container() {
       contentHeight = containerRef.current.clientHeight;
       const HeightOfBlocks = contentHeight / numberOfRows;
       const arr = [];
-      for (let a = 0; a < numberOfRows * 2; ++a) {
-        const question = generateMathQuestionForKids("+");
+      for (let a = 0; a < props.items.length; ++a) {
+        const question = props.items[a];
         arr.push(
           <MathBlock
             firstValue={question.operand1}
@@ -55,9 +23,8 @@ export function Container() {
         );
       }
       setItems(arr);
-      console.log(HeightOfBlocks);
     }
-  }, []);
+  }, [items]);
   return (
     <div ref={containerRef} className="content">
       {items}
